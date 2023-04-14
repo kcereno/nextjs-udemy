04b00d0004a814230af2c91a51ace8a88ec2cf30
Karl Cereno <kagc.dev@gmail.com>
2023-04-14 13:37:21 -0400

    Finish 8.144

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
-import EventList from '@/components/events/EventList';
-import { getFeaturedEvents } from '../helpers/api-util';
-import { Event } from '@/utils/interfaces';
-import Head from 'next/head';
-
 interface Props {
   featuredEvents: Event[];
 }
