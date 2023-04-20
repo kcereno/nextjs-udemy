01b56847c9b7e7426c800fa96e5135f17bd8ecc8
Karl Cereno <kagc.dev@gmail.com>
2023-04-20 12:57:14 -0400

    Finish Section 9" "

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -2,6 +2,18 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { MongoClient } from 'mongodb';
 import { addEmailtoDB } from '@/helpers/mongo';
 
+const connectDatabase = async () => {
+  const url =
+    'mongodb+srv://kcereno:kcereno@cluster0.jrx3t.mongodb.net/?retryWrites=true&w=majority';
+
+  return await MongoClient.connect(url);
+};
+
+const insertDocument = async (client: MongoClient, document: any) => {
+  const db = client.db();
+  await db.collection('newsletter').insertOne(document);
+};
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     {
@@ -13,7 +25,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       }
 
       // Store it in a database
-      addEmailtoDB(email);
+      let client;
+
+      try {
+        client = await connectDatabase();
+      } catch (error) {
+        res.status(500).json({ message: 'Failed to connect to DB' });
+        return;
+      }
+
+      try {
+        await insertDocument(client!, { email: email });
+        client!.close();
+      } catch (error) {
+        res.status(500).json({ message: 'Failed to insert data to DB' });
+        return;
+      }
+
       res.status(201).json({ message: 'Signed up!' });
     }
   }
