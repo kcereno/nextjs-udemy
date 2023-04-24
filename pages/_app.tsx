5d1898948339483b29a17898c43e7e2ec14217c4
Karl Cereno <kagc.dev@gmail.com>
2023-04-24 11:43:46 -0400

    Start section 11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,9 @@
-import Layout from '@/components/layout/Layout';
-import { NotificationContextProvider } from '@/store/notification-context';
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <NotificationContextProvider>
-      <Layout>
-        <Head>
-          <title>NextJS Events</title>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1"
-          />
-        </Head>
-
-        <Component {...pageProps} />
-      </Layout>
-    </NotificationContextProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,3 @@
-import EventList from '@/components/events/EventList';
-import { getFeaturedEvents } from '../helpers/api-util';
-import { Event } from '@/utils/interfaces';
-import Head from 'next/head';
-import NewsletterRegistration from '@/components/input/newsletter-registration';
-
 interface Props {
   featuredEvents: Event[];
 }
@@ -11,27 +5,7 @@ interface Props {
 export default function HomePage({ featuredEvents }: Props) {
   return (
     <>
-      <Head>
-        <title>NextJS Events</title>
-        <meta
-          name="description"
-          content="Find a lot of great events that allow you to evolve..."
-        />
-      </Head>
-      <NewsletterRegistration />
-      <EventList events={featuredEvents} />
+      <h1>Hello</h1>
     </>
   );
 }
-
-export const getStaticProps = async () => {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: { featuredEvents },
-    revalidate: 1800,
-  };
-};
-
-// TODO: handle newsletter registration. logs to console.
-// TODO: Submit comment to events
