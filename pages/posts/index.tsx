4f917c38ee02a7175bd77e8c54025a70003644b6
Karl Cereno <kagc.dev@gmail.com>
2023-04-25 12:54:15 -0400

    Finish 187

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,36 @@
+import AllPosts from '@/components/Posts/AllPosts/AllPosts';
 import React from 'react';
+import { PostI } from '../../models/interfaces';
 
-const AllPostsPage = () => {
-  return <div>AllPostsPage</div>;
+interface Props {
+  posts: PostI[];
+}
+
+const AllPostsPage = ({ posts }: Props) => {
+  const DUMMY_POSTS: PostI[] = [
+    {
+      title: 'Test',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'test',
+      slug: 'getting-started-nextjs',
+      date: new Date(),
+    },
+    {
+      title: 'Test',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'test',
+      slug: 'getting-started-nextjs',
+      date: new Date(),
+    },
+    {
+      title: 'Test',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'test',
+      slug: 'getting-started-nextjs',
+      date: new Date(),
+    },
+  ];
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
 export default AllPostsPage;
